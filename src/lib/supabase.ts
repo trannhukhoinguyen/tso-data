05c06efaccc155edfab991e58d76479135fdc58b
trann <trannhukhoinguyen@gmail.com>
2026-07-02 16:09:24 +0700

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Thay thế URL và ANON_KEY lấy từ mục Project Settings > API trên Supabase của bạn
-const supabaseUrl = 'https://your-supabase-url.supabase.co';
-const supabaseKey = 'your-anon-key-here';
+const supabaseUrl = 'https://gtppjhmtyyoxnsujtngg.supabase.co';
+const supabaseKey = 'sb_publishable_JZNOgyKv8u3xibeyFVeDtA_4K3JEQeP';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
